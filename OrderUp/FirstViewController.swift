96468c62cea3d8ee7e37f4bb6424fc341fb533ed
Zak Remer <zak.remer@gmail.com>
2017-09-21 12:36:28 -0700

    Initial commit.

---
+++ OrderUp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  OrderUp
+//
+//  Created by Zak Remer on 9/21/17.
+//  Copyright © 2017 Opal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
