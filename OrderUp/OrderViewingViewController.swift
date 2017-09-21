949e69dcb59b299547926f50dff2b413278d5646
Zak Remer <zak.remer@gmail.com>
2017-09-21 12:45:03 -0700

    Get ViewControllers organized.

---
+++ OrderUp/OrderViewingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.swift
+//  OrderUp
+//
+//  Created by Zak Remer on 9/21/17.
+//  Copyright © 2017 Opal. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewingViewController: UIViewController {
+
+}
+
